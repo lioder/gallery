2f59cd437c541b99a094ab5985cfe7debfecc83d
161250053 <161250053@smail.nju.edu.cn>
2018-02-14 21:42:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <photo :photo="photos[0]"></photo>
+  </div>
+</template>
+
+<script>
+  import photo from './components/photo/photo.vue'
+
+  const photos = require('./common/photos.json')
+  export default {
+    name: 'App',
+    data () {
+      return {
+        photos: photos
+      }
+    },
+    components: {
+      photo
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/photo/photo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="photo-wrapper">
+    <img :src="photo.url">
+    <div>{{ photo.desc }}</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      photo: {
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+
+</style>
