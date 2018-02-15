ad0c1a7b2082c17e510355980567843fad98d864
161250053 <161250053@smail.nju.edu.cn>
2018-02-15 14:09:40 +0800

    dev

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="app">
-    <photo :photo="photos[0]"></photo>
+    <header class="header">
+      <nav>
+        <div class="logo">
+          <img class="logo" src="./common/logo.png" width="40px" height="40px">
+        </div>
+        <span class="title">Time Lapse</span>
+        <ul class="menu">
+          <li class="list-item home"><a href="/">Home</a></li>
+          <li class="list-item archives"><a href="/archives/">Archives</a></li>
+          <li class="list-item projects"><a href="/projects/">Projects</a></li>
+          <li class="list-item gallery"><a href="/gallery/">Gallery</a></li>
+          <li class="list-item about"><a href="/about/">About</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div class="photos">
+      <photo class="photo" v-for="(photo,index) in photos" :photo="photo" :key="index"></photo>
+    </div>
+    <footer class="footer"><a href="http://lioder.top">© 2016-2018   Kry · L</a></footer>
   </div>
 </template>
 
@@ -21,13 +39,65 @@
   }
 </script>
 
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+<style lang="stylus">
+  @import "../static/css/reset.css";
+
+  #app
+    width: 100%
+    height: 100%
+    font-family: "PingFang SC", "Microsoft YaHei", STHeiti, "WenQuanYi Micro Hei", Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    background-color: #e1f9e7
+    color: #1E222E
+    .header
+      position: fixed
+      top: 0
+      left: 0
+      z-index: 200
+      width: 100%
+      height: 60px
+      line-height: 60px
+      background: rgba(255, 255, 255, 0.98)
+      box-shadow: 0 0 3px rgba(14, 14, 14, 0.26)
+      nav
+        padding: 0 80px
+        .logo
+          display: inline-block
+          padding-top: 5px
+          margin-right: 5px
+        .title
+          display: inline-block
+          transform: translateY(-10px)
+          line-height: 10px
+          font-size: 26px
+          color: #4f8b78
+        .menu
+          float: right
+          .list-item
+            display: inline-block
+            margin: 0 8px
+            a
+              font-size: 16px
+              color: #34495e
+              &:hover
+                color: #42b983
+    .photos
+      display: flex
+      flex-wrap: wrap
+      justify-content: center
+      padding: 100px 200px 0 200px
+      .photo
+        transform: translateX(20px)
+        flex: 0 0 auto
+        height: 198px
+        width: 195px
+        margin-bottom: 20px
+        margin-right: 20px
+    .footer
+      padding: 30px 0
+      text-align: center
+      line-height: 24px
+      a
+        color: #2a3e31
 </style>
--- src/components/photo/photo.vue
+++ src/components/photo/photo.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="photo-wrapper">
-    <img :src="photo.url">
-    <div>{{ photo.desc }}</div>
+    <img class="photo-content" :src="photo.url" width="180px" height="180px" @mouseenter="showDesc = true"
+         @mouseleave="showDesc = false">
+    <transition name="move">
+      <div class="photo-desc" v-show="showDesc">{{ photo.desc }}</div>
+    </transition>
   </div>
 </template>
 
@@ -11,10 +14,41 @@
       photo: {
         type: Object
       }
+    },
+    data () {
+      return {
+        showDesc: false
+      }
     }
   }
 </script>
 
 <style lang="stylus">
-
+  .photo-wrapper
+    display: inline-block
+    position: relative
+    width: 195px
+    height: 198px
+    text-align: center
+    overflow: hidden
+    background-color: #fff
+    .photo-content
+      padding: 9px 7.5px
+    .photo-desc
+      position: absolute
+      padding: 7.5px 0
+      left: 0
+      bottom: 0
+      height: 25.35px
+      width: 100%
+      transform: translateY(0)
+      background-color: rgba(255, 255, 255, 0.85)
+      text-align: center
+      font-size: 15px
+      color: #6E7799
+      &.move-enter-active, &.move-leave-active
+        transition: all 0.3s
+      &.move-enter, &.move-leave-to
+        transform: translateY(25.35px)
+        opacity: 0
 </style>
